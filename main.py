5c6a9dd72a93a16335d33b13bb7697f61d12c456
Leafan <la2player_07@mail.ru>
2023-04-28 21:41:06 +0300

    main add

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+
+def add_fild():
+
+    print('press button')
+
+window = tk.Tk()
+
+x, y = 0, 0
+
+entry = tk.Entry()
+entry.grid(column=0, row=x)
+
+btn = tk.Button(text='button', command=add_fild)
+btn.grid(column=0, row=x+1)
+
+
+window.mainloop()
+
